1dba633e7de06f07227b7c0ceb6256e13b24a477
Anthony Simone <anthony.simone7@gmail.com>
2020-02-12 21:44:31 -0800

    Move models to separate file

--- src/components/threejs/MapRenderer/modelHelpers.js
+++ src/components/threejs/MapRenderer/modelHelpers.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
+import { models } from './models'
+
 // Reusable onLoad function
 export const onModelLoad = (gltf, modelGroup, modelMatrix, mixers) => {
   const model = gltf.scene.children[0]
@@ -77,54 +79,6 @@ export const onModelProgress = () => {}
 // them to to console
 export const onModelError = errorMessage => {
   console.log(errorMessage)
-}
-
-const models = {
-  dad: {
-    url: '/threejs/models/90s_dad/scene.gltf',
-    normalizationScale: { x: 0.125, y: 0.125, z: 0.125 }
-  },
-  robot: {
-    url: '/threejs/models/RobotExpressive/RobotExpressive.glb',
-    normalizationScale: { x: 0.2, y: 0.2, z: 0.2 }
-  },
-  robotStatic: {
-    url: '/threejs/models/RobotExpressive/RobotStatic.glb',
-    normalizationScale: { x: 0.2, y: 0.2, z: 0.2 }
-  },
-  goblin: {
-    url: '/threejs/models/Goblin/Goblin.glb',
-    normalizationScale: { x: 0.2, y: 0.2, z: 0.2 }
-  },
-  bat: {
-    url: '/threejs/models/Monsters/Bat.glb',
-    normalizationScale: { x: 0.2, y: 0.2, z: 0.2 }
-  },
-  slime: {
-    url: '/threejs/models/Monsters/Slime.glb',
-    normalizationScale: { x: 0.2, y: 0.2, z: 0.2 }
-  },
-  dragon: {
-    url: '/threejs/models/Monsters/Dragon.glb',
-    normalizationScale: { x: 0.2, y: 0.2, z: 0.2 }
-  },
-  skeleton: {
-    url: '/threejs/models/Monsters/Skeleton.glb',
-    normalizationScale: { x: 0.2, y: 0.2, z: 0.2 }
-  },
-  // Dungeon pieces
-  tile: {
-    url: '/threejs/models/Dungeon_Modules/glb/Floor_Modular.glb',
-    normalizationScale: { x: 1, y: 1, z: 1 }
-  },
-  stairs: {
-    url: '/threejs/models/Dungeon_Modules/glb/Stairs_Modular.glb',
-    normalizationScale: { x: 1, y: 1, z: 1 }
-  },
-  wall: {
-    url: '/threejs/models/Dungeon_Modules/glb/Wall_Modular.glb',
-    normalizationScale: { x: 1, y: 1, z: 1 }
-  }
 }
 
 export function getModelUrl (key) {
---
+++ src/components/threejs/MapRenderer/models.js
@@ -0,0 +1,47 @@
+export const models = {
+  dad: {
+    url: '/threejs/models/90s_dad/scene.gltf',
+    normalizationScale: { x: 0.125, y: 0.125, z: 0.125 }
+  },
+  robot: {
+    url: '/threejs/models/RobotExpressive/RobotExpressive.glb',
+    normalizationScale: { x: 0.2, y: 0.2, z: 0.2 }
+  },
+  robotStatic: {
+    url: '/threejs/models/RobotExpressive/RobotStatic.glb',
+    normalizationScale: { x: 0.2, y: 0.2, z: 0.2 }
+  },
+  goblin: {
+    url: '/threejs/models/Goblin/Goblin.glb',
+    normalizationScale: { x: 0.2, y: 0.2, z: 0.2 }
+  },
+  bat: {
+    url: '/threejs/models/Monsters/Bat.glb',
+    normalizationScale: { x: 0.2, y: 0.2, z: 0.2 }
+  },
+  slime: {
+    url: '/threejs/models/Monsters/Slime.glb',
+    normalizationScale: { x: 0.2, y: 0.2, z: 0.2 }
+  },
+  dragon: {
+    url: '/threejs/models/Monsters/Dragon.glb',
+    normalizationScale: { x: 0.2, y: 0.2, z: 0.2 }
+  },
+  skeleton: {
+    url: '/threejs/models/Monsters/Skeleton.glb',
+    normalizationScale: { x: 0.2, y: 0.2, z: 0.2 }
+  },
+  // Dungeon pieces
+  tile: {
+    url: '/threejs/models/Dungeon_Modules/glb/Floor_Modular.glb',
+    normalizationScale: { x: 1, y: 1, z: 1 }
+  },
+  stairs: {
+    url: '/threejs/models/Dungeon_Modules/glb/Stairs_Modular.glb',
+    normalizationScale: { x: 1, y: 1, z: 1 }
+  },
+  wall: {
+    url: '/threejs/models/Dungeon_Modules/glb/Wall_Modular.glb',
+    normalizationScale: { x: 1, y: 1, z: 1 }
+  }
+}
